7886d0eb029c6d1175ff275b069ed17699b5f8b7
Phil Morgan <phil@psmorgan.co.uk>
2026-01-23 16:01:00 +0000

    PHAIN-222: Add ImportGmrMatch test (#15)
    This adds a test which covers off sending an ImportGmrMatch when we receive a MatchedGmr through.

---
+++ tests/TradeImportsGmr.JourneyTests/ImportGmrMatching/ImportMatchMessage.cs
@@ -0,0 +1,7 @@
+namespace TradeImportsGmr.JourneyTests.ImportGmrMatching;
+
+public class ImportMatchMessage
+{
+    public required string ImportReference { get; set; }
+    public required bool Match { get; set; }
+}
